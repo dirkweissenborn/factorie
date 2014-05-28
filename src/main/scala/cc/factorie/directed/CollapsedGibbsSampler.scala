31598899ddc4658f9f5ac40d468ba842fe676da2
dirk <dirk.weissenborn@gmail.com>
2014-05-28 20:44:05 +0200

    performance improvements and bug fix

--- src/main/scala/cc/factorie/directed/CollapsedGibbsSampler.scala
+++ src/main/scala/cc/factorie/directed/CollapsedGibbsSampler.scala
@@ -14,7 +14,7 @@
 package cc.factorie.directed
 
 import cc.factorie.infer._
-import scala.collection.mutable.{HashSet}
+import scala.collection.mutable.{ArrayBuffer, HashSet}
 import cc.factorie.variable._
 
 /** A GibbsSampler that can also collapse some Parameters. */
@@ -38,28 +38,23 @@ class CollapsedGibbsSampler(collapse:Iterable[Var], val model:DirectedModel)(imp
     vs.foreach{
       case v:MutableDiscreteVar =>
         val childFactors = model.childFactors(v)
-        val parentFactor = model.parentFactor(v)
+        val parentFactor = model.getParentFactor(v)
 
         val domainSize = v.domain.size
 
-        childFactors.map(f => if(f.parents.headOption.exists(isCollapsed)) f.updateCollapsedParents(-1.0))
-        if(parentFactor.parents.headOption.exists(isCollapsed)) parentFactor.updateCollapsedParents(-1.0)
+        val collapsedFactors = ArrayBuffer[DirectedFactor]()
+        collapsedFactors ++= childFactors.filter(f => f.parents.exists(isCollapsed))
+        if(parentFactor.isDefined && parentFactor.get.parents.exists(isCollapsed))
+          collapsedFactors += parentFactor.get
+
+        collapsedFactors.foreach(_.updateCollapsedParents(-1.0))
 
         var sum = 0.0
         val distribution = Array.ofDim[Double](domainSize)
         (0 until domainSize).foreach { value1 =>
-          val value = v.domain(value1)
-
-          val pValue = parentFactor match {
-            case f:DirectedFactorWithStatistics3[_,_,_] => f.pr(value.asInstanceOf[f._1.Value], f._2.value, f._3.value)
-            case f:DirectedFactorWithStatistics2[_,_] => f.pr(value.asInstanceOf[f._1.Value], f._2.value)
-            case f:DirectedFactorWithStatistics1[_] => f.pr(value.asInstanceOf[f._1.Value])
-          }
-
-          val cValue =  childFactors.foldLeft(0.0) {
-            case (acc:Double,f:DirectedFactorWithStatistics2[_,_]) => acc * f.pr(f._1.value, value.asInstanceOf[f._2.Value])
-            case (acc:Double,f:DirectedFactorWithStatistics3[_,_,_]) => acc * f.pr(f._1.value, f._2.value, value.asInstanceOf[f._3.Value])
-          }
+          v.set(value1)(null)
+          val pValue = parentFactor.map(_.pr).getOrElse(1.0)
+          val cValue = childFactors.foldLeft(1.0)(_ * _.pr)
 
           val pr = pValue * cValue
           sum += pr
@@ -69,41 +64,42 @@ class CollapsedGibbsSampler(collapse:Iterable[Var], val model:DirectedModel)(imp
         if (sum == 0) v.set(random.nextInt(domainSize))(null)
         else v.set(cc.factorie.maths.nextDiscrete(distribution, sum)(random))(null)
 
-        childFactors.map(f => if(f.parents.exists(isCollapsed)) f.updateCollapsedParents(1.0))
-        if(parentFactor.parents.headOption.exists(isCollapsed)) parentFactor.updateCollapsedParents(1.0)
+        collapsedFactors.foreach(_.updateCollapsedParents(1.0))
 
       case v:MutableDiscreteSeqVar[_] if !v.isEmpty =>       //TODO: make this more efficient
         val childFactors = model.childFactors(v)
-        val parentFactor = model.parentFactor(v)
+        val parentFactor = model.getParentFactor(v)
 
         val domainSize = v.domain.elementDomain.size
+
+        val collapsedFactors = ArrayBuffer[DirectedFactor]()
+        collapsedFactors ++= childFactors.filter(f => f.parents.exists(isCollapsed))
+        if(parentFactor.isDefined && parentFactor.get.parents.exists(isCollapsed))
+          collapsedFactors += parentFactor.get
 
         (0 until v.size).foreach(idx => {
           var sum = 0.0
           val distribution = Array.ofDim[Double](domainSize)
 
-          childFactors.map(f => if(f.parents.exists(isCollapsed)) f.updateCollapsedParentsForIdx(-1.0,idx))
-          if(parentFactor.parents.headOption.exists(isCollapsed)) parentFactor.updateCollapsedParentsForIdx(-1.0,idx)
+          collapsedFactors.foreach(f => f.updateCollapsedParentsForIdx(-1.0,idx))
 
           (0 until domainSize).foreach { value1 =>
-            lazy val value = v.value.updated(idx,v.domain.elementDomain(value1))  //don't compute if you do not have to
+            v.set(idx,value1)(null)
 
             val pValue = parentFactor match {
-              //make this faster by not calculating the whole probability here, because only the variable at idx changes
-              case f:PlatedDiscreteMixture.Factor => f._2(f._1(idx).intValue).value(value1)
-              case f:PlatedDiscrete.Factor => f._2.value(value1)
+              //faster by not calculating the whole probability here, because only the variable at idx changes
+              case Some(f:PlatedDiscreteMixture.Factor) => f._2(f._1(idx).intValue).value(value1)
+              case Some(f:PlatedDiscrete.Factor) => f._2.value(value1)
               //Defaults that could potentially be very slow if we are sampling a SeqVar (e.g., above two cases)
-              case f:DirectedFactorWithStatistics3[_,_,_] => f.pr(value.asInstanceOf[f._1.Value], f._2.value, f._3.value)
-              case f:DirectedFactorWithStatistics2[_,_] => f.pr(value.asInstanceOf[f._1.Value], f._2.value)
-              case f:DirectedFactorWithStatistics1[_] => f.pr(value.asInstanceOf[f._1.Value])
+              case Some(f:DirectedFactor) => f.pr
+              case None => 1.0
             }
 
             val cValue =  childFactors.foldLeft(1.0) {
               //make this faster by not calculating the whole probability here, because only the variable at idx changes
               case (acc:Double,f:PlatedDiscreteMixture.Factor) => acc * f._2(value1).value(f._1(idx).intValue)
               //Defaults that could potentially be very slow if we are sampling a SeqVar (e.g., above two cases)
-              case (acc:Double,f:DirectedFactorWithStatistics2[_,_]) => acc * f.pr(f.child.value, value.asInstanceOf[f._2.Value])
-              case (acc:Double,f:DirectedFactorWithStatistics3[_,_,_]) => acc * f.pr(f.child.value, f._2.value, value.asInstanceOf[f._3.Value])
+              case (acc:Double,f:DirectedFactor) => acc * f.pr
             }
 
             val pr = pValue * cValue
@@ -112,12 +108,9 @@ class CollapsedGibbsSampler(collapse:Iterable[Var], val model:DirectedModel)(imp
           }
           
           if (sum == 0) v.set(idx, random.nextInt(domainSize))(null)
-          else {
-            v.set(idx, cc.factorie.maths.nextDiscrete(distribution, sum)(random))(null)
-          }
+          else v.set(idx, cc.factorie.maths.nextDiscrete(distribution, sum)(random))(null)
 
-          childFactors.map(f => if(f.parents.exists(isCollapsed)) f.updateCollapsedParentsForIdx(1.0,idx))
-          if(parentFactor.parents.exists(isCollapsed)) parentFactor.updateCollapsedParentsForIdx(1.0,idx)
+          collapsedFactors.foreach(f => f.updateCollapsedParentsForIdx(1.0,idx))
         })
 
       case _ => throw new IllegalArgumentException("Can only sample for DiscreteVar or DiscreteSeqVar")
