2bd2d3a0ec380176c97e8b8aeba1048c7b5c93a7
dirk <dirk.weissenborn@gmail.com>
2014-06-03 10:46:00 +0200

    add DiscreteMixtureSeq.Factor case handling to DenseCountsProportionsMixtureCollapser

--- src/main/scala/cc/factorie/directed/Collapse.scala
+++ src/main/scala/cc/factorie/directed/Collapse.scala
@@ -90,6 +90,7 @@ object DenseCountsProportionsMixtureCollapser extends Collapser {
           //case f:MixtureComponent.Factor => {}
           case f: Mixture.Factor => {}
           case f: DiscreteMixture#Factor => m(f._3.intValue).value.masses.+=(f._1.intValue, 1.0)
+          case f: DiscreteMixtureSeq#Factor => f._1.intValues.foreach(childValue => m(f._3.intValue).value.masses.+=(childValue, 1.0))
           case f: PlatedDiscreteMixture.Factor => (0 until f._1.length).foreach(i => m(f._3(i).intValue).value.masses.+=(f._1(i).intValue, 1.0))
           case f: Factor => {
             println("DenseCountsProportionsMixtureCollapser unexpected factor " + f); return false
